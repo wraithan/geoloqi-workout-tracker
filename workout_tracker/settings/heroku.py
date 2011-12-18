27d54d1198f1c10d5a15223989d642d52d32ba65
Chris McDonald <XWraithanX@gmail.com>
2011-12-18 15:49:18 -0800

    "debugging"

--- workout_tracker/settings/heroku.py
+++ workout_tracker/settings/heroku.py
@@ -4,8 +4,11 @@ DATABASES = {}
 urlparse.uses_netloc.append('postgres')
 urlparse.uses_netloc.append('mysql')
 try:
+    print 'hai1'
     if os.environ.has_key('DATABASE_URL'):
+        print 'hai2'
         url = urlparse.urlparse(os.environ['DATABASE_URL'])
+        print 'hai3'
         DATABASES['default'] = {
             'NAME':     url.path[1:],
             'USER':     url.username,
@@ -13,9 +16,12 @@ try:
             'HOST':     url.hostname,
             'PORT':     url.port,
         }
+        print 'hai4'
         if url.scheme == 'postgres':
             DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+            print 'hai5'
         if url.scheme == 'mysql':
             DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+            print 'hai6'
 except:
-    print "Unexpected error:", sys.exc_info()
+    print 'FAIL'
